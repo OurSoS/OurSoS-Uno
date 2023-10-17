7ffc9e466ea7935a63f874a176a54b5c3f4d6483
paarthd00 <paarthcode@gmail.com>
2023-10-17 01:13:37 -0700

    Update User Language and Translation works

--- app/news.tsx
+++ app/news.tsx
@@ -95,7 +95,7 @@ export default function News() {
                         { flex: 1, marginLeft: 10 },
                       ]}
                     >
-                      <Link href={`/news/article/${news[i+1].title}`}>
+                      <Link href={`/news/article/${news[i + 1].title}`}>
                         <View style={styles.disasterInnerCard}>
                           <Image
                             source={{ uri: news[i + 1].thumbnail }}
