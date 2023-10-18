40f8c0a7828b2f5adbdbe0bcd7e22c8ac5d2646b
npaik <nakhyunpaik@gmail.com>
2023-10-18 09:44:23 -0700

    fix mistake

---
+++ components/searchBar.tsx
@@ -0,0 +1,13 @@
+// import { View, Text, Image, TextInput } from "react-native";
+// import { styles } from "../app/styles/newsStyles";
+
+// export default function SearchBarBox( title: string ) {
+//   return (
+//     <>
+//       <TextInput
+//         placeholder=
+//         style={styles.searchInput}
+//       ></TextInput>
+//     </>
+//   );
+// }
