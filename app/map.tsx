f3f1dc0d70835e8e5c55fa569f40c3beaa8b091e
JekBurri <onorbtanko@gmail.com>
2023-10-18 18:00:13 -0700

    deleted unused comments

--- app/map.tsx
+++ app/map.tsx
@@ -1,35 +1,3 @@
-// import React, { useState } from "react";
-// import { View, Text, Platform } from "react-native";
-// import MapView, { Marker } from "react-native-maps"; // Import the map component (assuming you have installed it)
-
-// export default function MapPage() {
-//   const [mapState, setMapState] = useState({
-//     latitude: 49.28346247273308,
-//     longitude: -123.11525937277163,
-//     latitudeDelta: 0.0922,
-//     longitudeDelta: 0.0421,
-//   });
-
-//   return (
-//     <View>
-//       <View>
-//         <Text>Map</Text>
-//       </View>
-//         <MapView
-//           style={{ flex: 1, height: 300 }} // Adjust the style as needed
-//           initialRegion={{
-//             latitude: mapState.latitude,
-//             longitude: mapState.longitude,
-//             latitudeDelta: mapState.latitudeDelta,
-//             longitudeDelta: mapState.longitudeDelta,
-//           }}
-//         >
-//         {/* <Marker coordinate={mapState} title={"jack"} /> */}
-//         </MapView>
-//     </View>
-//   );
-// }
-
 import React, { useState, useEffect } from "react";
 import MapView, { Marker } from "react-native-maps";
 import { StyleSheet, View, Text, TextInput } from "react-native";
