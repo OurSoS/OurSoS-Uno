b9af3173d00e255f83c1ad9984c28c41dab8722d
QubeQode <77304768+QubeQode@users.noreply.github.com>
2023-10-20 18:36:00 -0700

    Implement basic context state architecture for static content

--- app/index.tsx
+++ app/index.tsx
@@ -3,10 +3,58 @@ import { Link } from "expo-router";
 import React, { useState, useEffect } from "react";
 import * as Location from "expo-location";
 import IntroLayout from "./intro/_layout";
+import * as staticText from "../utils/static-text.json";
+import axios from "axios";
+import { StaticTextContext } from "./context/language-context";
+
+export type staticType = {
+  "intro-friends": {
+      "details": string,
+      "heading": string,
+  },
+  "intro-map": {
+      "details": string,
+      "heading": string,
+  },
+  "intro-newsfeed": {
+      "details": string,
+      "heading": string,
+  },
+  "map": {
+      "heading": string,
+  },
+  "news": {
+      "heading": string,
+      "search-placeholder": string,
+      "pins-header": string,
+      "friends-header": string,
+      "view-more": string,
+  },
+  "friends": {    
+      "search-placeholder": string,
+  },
+  "article": {
+      "details": string,
+  },
+  "button-text": {
+      "continue": string,
+      "back-button": string,
+  }
+}
 
 export default function App() {
 
+  // Setting app context to match the staticType data type so that context can be ref on other pgs
+  const [translatedStaticContent, setTranslatedStaticContent] = useState<staticType>(staticText);
+
+  // Axios call returns a translated object in type staticType for reference in remaining app
+  axios.post<staticType>("https://oursos-backend-production.up.railway.app/translateobject", staticText)
+  .then(res => {
+    setTranslatedStaticContent(res.data);
+  })
+
   return (
+    <StaticTextContext.Provider value={[translatedStaticContent, setTranslatedStaticContent]} >
     <View style={styles.container}>
       <IntroLayout>
         <Text style={styles.header}>Welcome to OurSoS!</Text>
@@ -19,6 +67,7 @@ export default function App() {
         <Link href="/news"><Text>Go News</Text></Link>
       </IntroLayout>
     </View>
+    </StaticTextContext.Provider>
   );
 }
 
