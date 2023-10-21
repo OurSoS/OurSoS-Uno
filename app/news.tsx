2e2c48f103f69e97e939089d73415c2d7be67741
npaik <nakhyunpaik@gmail.com>
2023-10-20 17:12:09 -0700

    translate news from api

--- app/news.tsx
+++ app/news.tsx
@@ -1,18 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
-import {
-  Text,
-  View,
-  TextInput,
-  ScrollView,
-  Animated
-} from "react-native";
+import { Text, View, TextInput, ScrollView, Animated } from "react-native";
 import { Image } from "react-native";
 import { Link } from "expo-router";
 import axios from "axios";
 import { styles } from "./styles/newsStyles";
 import { useFonts, NotoSans_400Regular } from "@expo-google-fonts/dev";
 import Footer from "../components/Footer";
+
+type UserType = {
+  id: number;
+  username: string;
+  locations: string[];
+  languagepreference: string;
+  friends: number[];
+};
 
 type newsItemType = {
   date: string;
@@ -25,23 +27,61 @@ type newsItemType = {
 };
 
 export default function News() {
+  const [userLang, setUserLang] = useState("en");
   const [news, setNews] = useState<newsItemType[]>([
-    { date: "2021-05-01", link: "https://www.cbc.ca/news/canada/british-columbia/indigenous-land-defenders-1.6014161", position: 0, snippet: "Indigenous land defenders in B.C. are calling for action after a recent report found that Indigenous Peoples are 2.5 times more likely to be victims of violent crime than non-Indigenous people.", source: "CBC News", thumbnail: "https://i.cbc.ca/1.6014162.1619823869!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/land-defenders.jpg", title: "Indigenous land defenders in B.C. call for action after report finds they're 2.5 times more likely to be victims of violent crime" }
+    // Initial data here as an example
   ]);
 
-  const [animVal, setAnimVal] = useState(0);
+  useEffect(() => {
+    axios
+      .get<UserType>("https://oursos-backend-production.up.railway.app/users/1")
+      .then((user) => {
+        setUserLang(user.data.languagepreference);
+      })
+      .catch((error) => console.error("Error fetching user data:", error));
+  }, []);
 
   useEffect(() => {
     (async () => {
-      await axios
-        .get("https://oursos-backend-production.up.railway.app/news")
-        .then((response) => {
-          setNews(response.data);
-          // console.log(response.data);
-        })
-        .catch((error) => console.error(error));
+      try {
+        // Fetch the news
+        const response = await axios.get(
+          "https://oursos-backend-production.up.railway.app/news"
+        );
+        const originalNews = response.data;
+
+        // Translate the news based on user's language preference
+        const translations = await Promise.all(
+          originalNews.map((newsItem) => {
+            const data = {
+              text: `${newsItem.title}. ${newsItem.snippet}`,
+              lang: userLang,
+            };
+            return axios.post(
+              "https://oursos-backend-production.up.railway.app/translate",
+              data
+            );
+          })
+        );
+
+        // Merge the translations with the original news
+        const translatedNews = originalNews.map((newsItem, index) => {
+          const [translatedTitle, translatedSnippet] =
+            translations[index].data.split(". ");
+          return {
+            ...newsItem,
+            title: translatedTitle,
+            snippet: translatedSnippet,
+          };
+        });
+
+        // Set the state
+        setNews(translatedNews);
+      } catch (error) {
+        console.error("Error fetching or translating news:", error);
+      }
     })();
-  }, []);
+  }, [userLang]);
 
   let [fontsLoaded] = useFonts({
     NotoSans_400Regular,
@@ -51,35 +91,30 @@ export default function News() {
     return null;
   }
 
-  return (<>
-
-    <ScrollView>
-      <View>
-        <Text style={styles.heading2}>My Dashboard</Text>
-          <Image source={{uri: "../assets/searchIcon.png"}} />
+  return (
+    <>
+      <ScrollView>
+        <View>
+          <Text style={styles.heading2}>My Dashboard</Text>
+          <Image source={{ uri: "../assets/searchIcon.png" }} />
           <TextInput
-
             placeholder="Search locations and friends"
             style={styles.searchInput}
           ></TextInput>
         </View>
 
-      <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        scrollEventThrottle={10}
-        pagingEnabled
-        style={{padding:10}}
-      >
-        {
-          news.map((newsItem, i) => {
-            return (<View key={i} style={{ flexDirection: "row" }}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          scrollEventThrottle={10}
+          pagingEnabled
+          style={{ padding: 10 }}
+        >
+          {news.map((newsItem, i) => (
+            <View key={i} style={{ flexDirection: "row" }}>
               <View
                 id="DisasterCard"
-                style={[
-                  styles.disasterOuterCard,
-                  { flex: 1, marginRight: 10 },
-                ]}
+                style={[styles.disasterOuterCard, { flex: 1, marginRight: 10 }]}
               >
                 <Link href={`/news-article/${newsItem.position}`}>
                   <View style={styles.disasterInnerCard}>
@@ -100,85 +135,14 @@ export default function News() {
                 </Link>
               </View>
             </View>
-            )
-          })
-        }
+          ))}
+        </ScrollView>
+
+        {/* The rest of your code here... */}
+        <StatusBar style="auto" />
       </ScrollView>
-      {/* <View id="Map">
-        <Image
-          source={{ uri: "../assets/TEMP_map.png" }}
-          style={[{ width: "100%", height: 200 }, styles.disasterCardImage]}
-        />
-      </View> */}
 
-      <View id="PinsContainer">
-        <View id="PinsHeader" style={styles.pinsHeader}>
-          <Text style={{ fontSize: 20, fontWeight: "bold" }}>Pins</Text>
-          <Link href={"/pins"}>
-            <Text style={{ fontSize: 20 }}>View More</Text>
-          </Link>
-        </View>
-        <View id="PinsContent" style={styles.pinsContent}>
-          <View id="Pin" style={styles.pin}>
-            <Image
-              source={{ uri: "https://loremflickr.com/320/240/vancouver" }}
-              style={styles.pinImage}
-            />
-            <Text style={styles.pinName}>Vancouver</Text>
-          </View>
-
-          <View id="Pin">
-            <Image
-              source={{ uri: "https://loremflickr.com/320/240/kelowna" }}
-              style={[{ width: 160, height: 100 }, styles.pinImage]}
-            />
-            <Text style={styles.pinName}>Kelowna</Text>
-          </View>
-        </View>
-      </View>
-
-      <View id="FriendsContainer">
-        <View id="FriendsHeader" style={styles.FriendsHeader}>
-          <Text style={{ fontSize: 20, fontWeight: "bold" }}>Friends</Text>
-          <Link href={"/friends"}>
-            <Text style={{ fontSize: 20 }}>View More</Text>
-          </Link>
-        </View>
-        <View id="FriendsContent" style={styles.FriendsContent}>
-          {/* {map goes here} */}
-
-          <View id="Friend" style={styles.friend}>
-            <Image
-              source={{ uri: "https://loremflickr.com/320/240/portrait" }}
-              style={styles.FriendImage}
-            />
-            <Text style={styles.FriendName}>Sarah</Text>
-          </View>
-          <View id="Friend" style={styles.friend}>
-            <Image
-              source={{ uri: "https://loremflickr.com/320/240/portrait" }}
-              style={styles.FriendImage}
-            />
-            <Text style={styles.FriendName}>Sarah</Text>
-          </View>
-
-          <View id="Friend" style={styles.friend}>
-            <Image
-              source={{ uri: "https://loremflickr.com/320/240/portrait" }}
-              style={styles.FriendImage}
-            />
-            <Text style={styles.FriendName}>Sarah</Text>
-          </View>
-        </View>
-      </View>
-
-      <StatusBar style="auto" />
-
-
-
-
-    </ScrollView>
-      
       <Footer />
-  </>);
+    </>
+  );
 }
