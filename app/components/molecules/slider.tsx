cc4abab7b814d5872c2f9a7c16f68bd40109b0f1
npaik <nakhyunpaik@gmail.com>
2023-11-01 11:04:19 -0700

    translate + merge conflix fix

--- app/components/molecules/slider.tsx
+++ app/components/molecules/slider.tsx
@@ -1,28 +1,45 @@
-import { View, Image, Text, TextInput, ScrollView, SafeAreaView } from "react-native";
-import { useEffect, useState } from "react";
-import tw from 'twrnc';
+import React, { useEffect } from "react";
+import { ScrollView } from "react-native";
 import ContentCard from "./content-card";
 
-export default function Slider({ data, translatedData, onToggleSnackBar }: { data: any[], translatedData: any, onToggleSnackBar: any }) {
-        useEffect(() => {
-                // console.log(translatedData);
-        }, [])
+export default function Slider({
+  data,
+  translatedData,
+  onToggleSnackBar,
+}: {
+  data: any[];
+  translatedData: any;
+  onToggleSnackBar: any;
+}) {
+  useEffect(() => {
+    console.log("Translated Data: ", translatedData);
+  }, [translatedData]);
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      scrollEventThrottle={10}
+      pagingEnabled
+      style={{ padding: 10 }}
+      contentContainerStyle={{ justifyContent: "center" }}
+    >
+      {data.map((item, i) => {
+        const displayData =
+          translatedData && translatedData[i] ? translatedData[i] : item;
+
+        console.log(`Item at index ${i}:`, item, `; Translated:`, displayData);
+
         return (
-                <ScrollView
-                        horizontal
-                        showsHorizontalScrollIndicator={false}
-                        scrollEventThrottle={10}
-                        pagingEnabled
-                        style={{ padding: 10 }}
-                        contentContainerStyle={{ justifyContent: 'center' }}
-                >
-                        {
-                                data.map((data, i) => {
-                                        return (
-                                                <ContentCard onToggleSnackBar={onToggleSnackBar} key={i} data={data} imgSrc={data.thumbnail} heading={data?.title} snippet={data.snippet} />
-                                        )
-                                })
-                        }
-                </ScrollView>
-        )
+          <ContentCard
+            onToggleSnackBar={onToggleSnackBar}
+            key={i}
+            data={item}
+            imgSrc={item.thumbnail}
+            heading={displayData.title}
+            snippet={displayData.snippet}
+          />
+        );
+      })}
+    </ScrollView>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,50 +3,46 @@ import { Link } from "expo-router";
 import React, { useState, useEffect, useContext } from "react";
 import * as Location from "expo-location";
 import axios from "axios";
-import staticText from "../utils/static-text.json"
-import { UserLanguageContext } from "./context/language-context";
-
+import staticText from "../utils/static-text.json";
 import IntroTextButton from "./components/intro/intro-text-button";
 import IntroLayout from "./components/intro/_layout";
-
-import tw from 'twrnc';
+import tw from "twrnc";
 import Dashboard from "./components/dashboard/dashboard";
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export type staticType = {
   "intro-friends": {
-    "details": string,
-    "heading": string,
-  },
+    details: string;
+    heading: string;
+  };
   "intro-map": {
-    "details": string,
-    "heading": string,
-  },
+    details: string;
+    heading: string;
+  };
   "intro-newsfeed": {
-    "details": string,
-    "heading": string,
-  },
-  "map": {
-    "heading": string,
-    "search-placeholder": string,
-  },
-  "news": {
-    "heading": string,
-    "search-placeholder": string,
-    "pins-header": string,
-    "friends-header": string,
-    "view-more": string,
-  },
-  "friends": {
-    "search-placeholder": string,
-  },
-  "article": {
-    "details": string,
-  },
+    details: string;
+    heading: string;
+  };
+  map: {
+    heading: string;
+    "search-placeholder": string;
+  };
+  news: {
+    heading: string;
+    "search-placeholder": string;
+    "pins-header": string;
+    "friends-header": string;
+    "view-more": string;
+  };
+  friends: {
+    "search-placeholder": string;
+  };
+  article: {
+    details: string;
+  };
   "button-text": {
-    "continue": string,
-    "back-button": string,
-  }
+    continue: string;
+    "back-button": string;
+  };
 };
 
 type LanguageType = {
@@ -55,41 +51,39 @@ type LanguageType = {
 };
 
 export default function Index() {
-  const [translatedStaticContent, setTranslatedStaticContent] = useState<any>(staticText);
+  const [translatedStaticContent, setTranslatedStaticContent] =
+    useState<any>(staticText);
   const [userLang, setUserLang] = useState("hi");
   const [introComponent, setIntroComponent] = useState("welcome");
-  const [languages, setLanguages] = useState<LanguageType[]>([])
+  const [languages, setLanguages] = useState<LanguageType[]>([]);
   const [location, setLocation] = useState<any>(null);
   const [errorMsg, setErrorMsg] = useState<string>("");
   const [currentUser, setCurrentUser] = useState<any>(null);
   const setUserLanguage = async () => {
     const updateUserRequest = {
-      "username": "sam",
-      "latitude": 49.26357,
-      "longitude": -123.13857,
-      "languagepreference": userLang, // Ensure that languageTag is defined and has a valid value
-      "friends": [2, 3],
-      "profile": "https://picsum.photos/200/300?grayscale"
+      username: "sam",
+      latitude: 49.26357,
+      longitude: -123.13857,
+      languagepreference: userLang, // Ensure that languageTag is defined and has a valid value
+      friends: [2, 3],
+      profile: "https://picsum.photos/200/300?grayscale",
     };
     if (userLang) {
       setUserLang(userLang);
-      // await axios
-      //   .put(
-      //     "https://oursos-backend-production.up.railway.app/updateuser/1",
-      //     updateUserRequest
-      //   )
-      //   .then(async (response) => {
-      await axios.post<{ "translateObject": staticType, "lang": string }>("https://oursos-backend-production.up.railway.app/translateobject", { "translateObject": staticText, "lang": userLang })
-        .then(res => {
+
+      await axios
+        .post<{ translateObject: staticType; lang: string }>(
+          "https://oursos-backend-production.up.railway.app/translateobject",
+          { translateObject: staticText, lang: userLang }
+        )
+        .then((res) => {
           setTranslatedStaticContent(res.data);
-          setIntroComponent("newsFeed")
-        })
-      // })
+          setIntroComponent("newsFeed");
+        });
     }
   };
 
   useEffect(() => {
-
     (async () => {
       await axios
         .get("https://oursos-backend-production.up.railway.app/languages")
@@ -98,8 +92,8 @@ export default function Index() {
         });
 
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
         return;
       }
 
@@ -115,87 +109,98 @@ export default function Index() {
 
   const buttonFunction = (buttonText: string) => {
     setIntroComponent(buttonText);
-  }
+  };
 
-  let text = 'Waiting..';
+  let text = "Waiting..";
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
     text = JSON.stringify(location);
   }
   return (
-    // <Text>fooo</Text>
-    // <UserLanguageContext.Provider value={[userLang, setUserLang]}>
-    // <SafeAreaProvider>
     <View style={styles.container}>
-      {
-        introComponent === "welcome" ?
-          <IntroTextButton heading="Welcome To OurSOS!"
-            details="Empowering Your Safety, Connecting Our World"
-            buttonNext="selectLocation"
-            buttonText="Select Language"
-            buttonFunction={buttonFunction}
-          >
-          </IntroTextButton>
-          : introComponent === "selectLocation" ?
-            <IntroLayout>
-              <Text style={styles.header}>Select your language</Text>
-              <FlatList
-                style={tw.style(`w-full`, `flex`, `flex-col`)}
-                data={languages}
-                renderItem={({
-                  item,
-                  index,
-                }: {
-                  item: LanguageType;
-                  index: number;
-                }) => (
-                  <Pressable
-                    onPress={() => {
-                      setUserLang(languages[index]?.tag);
-                    }}
-                    style={tw.style(`text-white`, `bg-white`, `px-7`, `py-3`, `rounded-lg`, `border`, `mb-3`)}
-                  >
-                    <Text style={styles.text}>{item.name}</Text>
-                  </Pressable>
-                )}
-              />
+      {introComponent === "welcome" ? (
+        <IntroTextButton
+          heading="Welcome To OurSOS!"
+          details="Empowering Your Safety, Connecting Our World"
+          buttonNext="selectLocation"
+          buttonText="Select Language"
+          buttonFunction={buttonFunction}
+        ></IntroTextButton>
+      ) : introComponent === "selectLocation" ? (
+        <IntroLayout>
+          <Text style={styles.header}>Select your language</Text>
+          <FlatList
+            style={tw.style(`w-full`, `flex`, `flex-col`)}
+            data={languages}
+            renderItem={({
+              item,
+              index,
+            }: {
+              item: LanguageType;
+              index: number;
+            }) => (
               <Pressable
                 onPress={() => {
-                  setUserLanguage();
+                  setUserLang(languages[index]?.tag);
                 }}
-                style={tw.style(`text-white`, `bg-[#003566]`, `px-7`, `py-3`, `rounded-lg`)}
+                style={tw.style(
+                  `text-white`,
+                  `bg-white`,
+                  `px-7`,
+                  `py-3`,
+                  `rounded-lg`,
+                  `border`,
+                  `mb-3`
+                )}
               >
-                <Text style={tw.style(`text-white`)}>Continue</Text>
+                <Text style={styles.text}>{item.name}</Text>
               </Pressable>
-            </IntroLayout>
-            : introComponent === "newsFeed"
-              ? <IntroTextButton heading={translatedStaticContent["intro-newsfeed"].heading}
-                details={translatedStaticContent["intro-newsfeed"].details}
-                buttonNext="introMap"
-                buttonText={translatedStaticContent["button-text"].continue}
-                buttonFunction={buttonFunction}
-              ></IntroTextButton>
-              : introComponent === "introMap"
-                ? <IntroTextButton heading={translatedStaticContent["intro-map"].heading}
-                  details={translatedStaticContent["intro-map"].details}
-                  buttonNext="introFriends"
-                  buttonText={translatedStaticContent["button-text"].continue}
-                  buttonFunction={buttonFunction}
-                ></IntroTextButton>
-                : introComponent === "introFriends"
-                  ? <IntroTextButton heading={translatedStaticContent["intro-friends"].heading}
-                    details={translatedStaticContent["intro-friends"].details}
-                    buttonNext="dashboard"
-                    buttonText={translatedStaticContent["button-text"].continue}
-
-                    buttonFunction={buttonFunction}
-                  ></IntroTextButton>
-                  : <Dashboard user={currentUser} userLang={userLang}></Dashboard>
-      }
+            )}
+          />
+          <Pressable
+            onPress={() => {
+              setUserLanguage();
+            }}
+            style={tw.style(
+              `text-white`,
+              `bg-[#003566]`,
+              `px-7`,
+              `py-3`,
+              `rounded-lg`
+            )}
+          >
+            <Text style={tw.style(`text-white`)}>Continue</Text>
+          </Pressable>
+        </IntroLayout>
+      ) : introComponent === "newsFeed" ? (
+        <IntroTextButton
+          heading={translatedStaticContent["intro-newsfeed"].heading}
+          details={translatedStaticContent["intro-newsfeed"].details}
+          buttonNext="introMap"
+          buttonText={translatedStaticContent["button-text"].continue}
+          buttonFunction={buttonFunction}
+        ></IntroTextButton>
+      ) : introComponent === "introMap" ? (
+        <IntroTextButton
+          heading={translatedStaticContent["intro-map"].heading}
+          details={translatedStaticContent["intro-map"].details}
+          buttonNext="introFriends"
+          buttonText={translatedStaticContent["button-text"].continue}
+          buttonFunction={buttonFunction}
+        ></IntroTextButton>
+      ) : introComponent === "introFriends" ? (
+        <IntroTextButton
+          heading={translatedStaticContent["intro-friends"].heading}
+          details={translatedStaticContent["intro-friends"].details}
+          buttonNext="dashboard"
+          buttonText={translatedStaticContent["button-text"].continue}
+          buttonFunction={buttonFunction}
+        ></IntroTextButton>
+      ) : (
+        <Dashboard user={currentUser} userLang={userLang}></Dashboard>
+      )}
     </View>
-    // </SafeAreaProvider>
-    // </UserLanguageContext.Provider>
   );
 }
 
@@ -242,5 +247,5 @@ const styles = StyleSheet.create({
     lineHeight: 21,
     letterSpacing: 0.25,
     color: "black",
-  }
+  },
 });
