93ca70c1558a6cd2f2fb62f5f35bc64abe586140
“hxofficial” <hxofficialmail@gmail.com>
2023-11-20 12:49:22 -0800

    Add useState for friends in dashboards.tsx

--- app/components/dashboard/dashboard.tsx
+++ app/components/dashboard/dashboard.tsx
@@ -34,6 +34,7 @@ export default function Dashboard({
   const [visible, setVisible] = useState(false);
   const [currentUser, setCurrentUser] = useState(user);
   const [pins, setPins] = useState(user?.locations);
+  const [friends, setFriends] = useState(user?.friends);
   const [snackData, setSnackData] = useState<any>({});
   const onToggleSnackBar = (data: any) => {
     setSnackData(data);
