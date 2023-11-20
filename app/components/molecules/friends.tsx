12faa7a1255a9b6d4e9779afa0f185ccfd133f5e
“hxofficial” <hxofficialmail@gmail.com>
2023-11-20 12:30:14 -0800

    Avatar.png path added to each person in friends data

--- app/components/molecules/friends.tsx
+++ app/components/molecules/friends.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 
 const friends = [
-  { id: 1, name: "Hani", image: require("profile") },
-  { id: 2, name: "Moh", image: require("") },
-  { id: 3, name: "Paarth", image: require("") },
-  { id: 4, name: "Tony", image: require("") },
-  { id: 5, name: "Jack", image: require("") },
-  { id: 6, name: "Neema", image: require("") },
+  { id: 1, name: "Hani", image: require("../../../assets/avatars/Avatar.png") },
+  { id: 2, name: "Moh", image: require("../../../assets/avatars/Avatar.png") },
+  {
+    id: 3,
+    name: "Paarth",
+    image: require("../../../assets/avatars/Avatar.png"),
+  },
+  { id: 4, name: "Tony", image: require("../../../assets/avatars/Avatar.png") },
+  { id: 5, name: "Jack", image: require("../../../assets/avatars/Avatar.png") },
+  {
+    id: 6,
+    name: "Neema",
+    image: require("../../../assets/avatars/Avatar.png"),
+  },
 ];
 
 const FriendsList = () => {
