7b569f1074ba823404101ed30a5353b50a4ae28a
“hxofficial” <hxofficialmail@gmail.com>
2023-11-20 12:24:12 -0800

    Initialize FriendsList Component

---
+++ app/components/molecules/friends.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+
+const FriendsList = () => {
+  return <View></View>;
+};
+
+export default FriendsList;
