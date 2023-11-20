e44914ae3d011a15324f341f6add32d7454217f2
“hxofficial” <hxofficialmail@gmail.com>
2023-11-20 12:26:04 -0800

    Add Friends Data

--- app/components/molecules/friends.tsx
+++ app/components/molecules/friends.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 
+const friends = [
+  { id: 1, name: "Hani", image: require("profile") },
+  { id: 2, name: "Moh", image: require("") },
+  { id: 3, name: "Paarth", image: require("") },
+  { id: 4, name: "Tony", image: require("") },
+  { id: 5, name: "Jack", image: require("") },
+  { id: 6, name: "Neema", image: require("") },
+];
+
 const FriendsList = () => {
   return <View></View>;
 };
