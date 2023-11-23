5f100efc97d5591496fb5157ff498d60ee277ef2
JekBurri <onorbtanko@gmail.com>
2023-11-22 19:43:33 -0800

    created a modal for the map in a feed view format

--- app/components/modalViewAlerts.tsx
+++ app/components/modalViewAlerts.tsx
@@ -3,11 +3,21 @@ import { View, Text, Image, Pressable } from "react-native";
 import tw from "twrnc";
 
 type ModalViewAlertsProps = {
-        data: any;
-        handleGoToMap: (latitude: string, longitude: string) => void; 
-      };
-      
-const ModalViewAlerts = (props: ModalViewAlertsProps) => {
+  data: any;
+  type: "Fire" | "Earthquake" | "Tsunami";
+  setJumpToLocation: React.Dispatch<
+    React.SetStateAction<{
+      longitude: number;
+      latitude: number;
+    }>
+  >;
+  jumpToLocation: {
+    longitude: number;
+    latitude: number;
+  };
+};
+
+const ModalViewAlerts = React.memo((props: ModalViewAlertsProps) => {
   return (
     <View>
       {props.data.map((a: any, index: number) => (
@@ -18,15 +28,43 @@ const ModalViewAlerts = (props: ModalViewAlertsProps) => {
           )}
         >
           <View style={tw.style("flex flex-row p-2 m-2")}>
-            <Image
-              source={require("../../assets/mapIcons/Wildfire.png")}
-              style={tw.style("w-10 h-10 mr-2 mt-1 self-center")}
-            />
+            {props.type === "Fire" ? (
+              <Image
+                source={require("../../assets/mapIcons/Wildfire.png")}
+                style={tw.style("w-10 h-10 mr-2 mt-1 self-center")}
+              />
+            ) : props.type === "Earthquake" ? (
+              <Image
+                source={require("../../assets/mapIcons/Earthquake.png")}
+                style={tw.style("w-10 h-10 mr-2 mt-1 self-center")}
+              />
+            ) : props.type === "Tsunami" ? (
+              <Image
+                source={require("../../assets/mapIcons/Tsunami.png")}
+                style={tw.style("w-10 h-10 mr-2 mt-1 self-center")}
+              />
+            ) : null}
+
             <View style={tw.style("flex-1")}>
-              <Text style={tw.style("font-bold text-sm mb-1")}>Fire</Text>
-              <Text style={tw.style("text-sm")}>
-                Location - {typeof a?.latitude}, {a?.longitude}
+              <Text style={tw.style("font-bold text-sm mb-1")}>
+                {props.type}
               </Text>
+
+              {props.type === "Fire" ? (
+                <Text style={tw.style("text-sm")}>
+                  Location - {a?.latitude}, {a?.longitude}
+                </Text>
+              ) : props.type === "Earthquake" ? (
+                <Text style={tw.style("text-sm")}>
+                  Location - {a?.geometry.coordinates[1].toFixed(2)},{" "}
+                  {a?.geometry.coordinates[0].toFixed(2)}
+                </Text>
+              ) : props.type === "Tsunami" ? (
+                <Text style={tw.style("text-sm")}>
+                  Location - {a?.latitude}, {a?.longitude}
+                </Text>
+              ) : null}
+
               <Text style={tw.style("text-sm")}>
                 Severity - Calculated on backend
               </Text>
@@ -37,7 +75,15 @@ const ModalViewAlerts = (props: ModalViewAlertsProps) => {
           </View>
           <Pressable
             style={tw.style("bg-blue-500 p-2 rounded-md mt-2 self-center")}
-            onPress={() => props.handleGoToMap(a.latitude, a.longitude)}
+            onPress={() => {
+              props.setJumpToLocation((prevLocation) => ({
+                ...prevLocation,
+                longitude: a.longitude,
+                latitude: a.latitude,
+            }));
+            console.log(a.latitude, a.longitude);
+            console.log(props.jumpToLocation);
+            }}
           >
             <Text style={tw.style("text-white text-sm")}>View on Map</Text>
           </Pressable>
@@ -45,6 +91,6 @@ const ModalViewAlerts = (props: ModalViewAlertsProps) => {
       ))}
     </View>
   );
-};
+});
 
 export default ModalViewAlerts;
