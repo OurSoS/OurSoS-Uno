d1414ff6aa2e11a5f17aa76a8436ff3a664aca6f
JekBurri <onorbtanko@gmail.com>
2023-11-26 21:49:10 -0800

    Refactor modalViewAlerts component to use the Alert type

--- app/components/modalViewAlerts.tsx
+++ app/components/modalViewAlerts.tsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { View, Text, Image, Pressable } from "react-native";
+import { View, Text, Image } from "react-native";
 import tw from "twrnc";
 
+type Alert = {
+  type: string;
+  latitude: number;
+  longitude: number;
+  geometry?: {
+    coordinates: [number, number];
+  };
+  acq_time?: string;
+  acq_date?: string;
+  scan?: string;
+  message?: string;
+  radius?: string;
+};
+
 type ModalViewAlertsProps = {
-  data: any;
-  type: "Fire" | "Earthquake" | "Tsunami" | "User Alert";
+  data: Alert[];
   setJumpToLocation: React.Dispatch<
     React.SetStateAction<{
       longitude: number;
@@ -15,12 +28,14 @@ type ModalViewAlertsProps = {
     longitude: number;
     latitude: number;
   };
+  type: string;
 };
 
 const ModalViewAlerts = React.memo((props: ModalViewAlertsProps) => {
+  console.log(props.data)
   return (
     <View style={tw.style("pl-2 pr-18")}>
-      {props.data.map((a: any, index: number) => (
+      {props.data.map((alert: Alert, index: number) => (
         <View
           key={index}
           style={tw.style(
@@ -28,126 +43,107 @@ const ModalViewAlerts = React.memo((props: ModalViewAlertsProps) => {
           )}
         >
           <View style={tw.style("flex flex-row p-2 m-2")}>
-            {props.type === "Fire" ? (
-              <Image
-                source={require("../../assets/mapIcons/Wildfire.png")}
-                style={tw.style("w-10 h-10 mr-2 mt-1 self-center")}
-              />
-            ) : props.type === "Earthquake" ? (
-              <Image
-                source={require("../../assets/mapIcons/Earthquake.png")}
-                style={tw.style("w-10 h-10 mr-2 mt-1 self-center")}
-              />
-            ) : props.type === "Tsunami" ? (
-              <Image
-                source={require("../../assets/mapIcons/Tsunami.png")}
-                style={tw.style("w-10 h-10 mr-2 mt-1 self-center")}
-              />
-            ) : props.type === "User Alert" ? (
-              <Image
-                source={a.type === "Hazard" ? require("../../assets/alert-categorys/Hazard.png") :
-                        a.type === "Fire" ? require("../../assets/alert-categorys/Fire.png") :
-                        a.type === "Police" ? require("../../assets/alert-categorys/Police.png") : null}
-                style={tw.style("w-10 h-10 mr-2 mt-1 self-center")}
-              />
-            ) : null}
+            <Image
+              source={getAlertIcon(alert.type)}
+              style={tw.style("w-10 h-10 mr-2 mt-1 self-center")}
+            />
 
             <View style={tw.style("flex-1")}>
               <Text style={tw.style("font-bold text-sm mb-1 text-white")}>
-                {props.type}
+                {alert.type}
               </Text>
 
-              {props.type === "Fire" ? (
+              {alert.type === "Fire" ? (
                 <Text style={tw.style("text-sm text-white")}>
-                  Location - {a?.latitude}, {a?.longitude}
+                  Location - {alert.latitude}, {alert.longitude}
                 </Text>
-              ) : props.type === "Earthquake" ? (
+              ) : alert.type === "Earthquake" ? (
                 <Text style={tw.style("text-sm text-white")}>
-                  Location - {a?.geometry.coordinates[1].toFixed(2)},{" "}
-                  {a?.geometry.coordinates[0].toFixed(2)}
+                  Location - {alert.geometry?.coordinates[1].toFixed(2)},{" "}
+                  {alert.geometry?.coordinates[0].toFixed(2)}
                 </Text>
-              ) : props.type === "Tsunami" ? (
+              ) : alert.type === "Tsunami" ? (
                 <Text style={tw.style("text-sm text-white")}>
-                  Location - {a?.latitude}, {a?.longitude}
+                  Location - {alert.latitude}, {alert.longitude}
                 </Text>
-              ) : props.type === "User Alert" ? (
+              ) : alert.type === "User Alert" ? (
                 <Text style={tw.style("text-sm text-white")}>
-                  Location - {a?.latitude}, {a?.longitude}
+                  Location - {alert.latitude}, {alert.longitude}
                 </Text>
               ) : null}
 
-              {/* <Text style={tw.style("text-sm text-white")}>
-                Severity - Calculated on backend
-              </Text> */}
-              {props.type === "Fire" ? (
+              {alert.type === "Fire" ? (
                 <>
-                  <Text style={tw.style("text-sm text-white")}>Time - {a?.acq_time}</Text>
-                  <Text style={tw.style("text-sm text-white")}>Date - {a?.acq_date}</Text>
-                  <Text style={tw.style("text-sm text-white")}>Radius - {a?.scan}</Text>
+                  <Text style={tw.style("text-sm text-white")}>Time - {alert.acq_time}</Text>
+                  <Text style={tw.style("text-sm text-white")}>Date - {alert.acq_date}</Text>
+                  <Text style={tw.style("text-sm text-white")}>Radius - {alert.scan}</Text>
 
                   {/* Add a View to represent the circle */}
-                  {typeof a?.scan === "string" &&
-                    !isNaN(parseFloat(a?.scan)) && (
+                  {typeof alert.scan === "string" &&
+                    !isNaN(parseFloat(alert.scan)) && (
                       <View
                         style={{
-                          width: parseFloat(a?.scan) * 2, // Diameter of the circle
-                          height: parseFloat(a?.scan) * 2,
-                          borderRadius: parseFloat(a?.scan), // Set borderRadius to half of the width/height to make it a circle
-                          backgroundColor: "orange", // Change the color as needed
+                          width: parseFloat(alert.scan) * 2,
+                          height: parseFloat(alert.scan) * 2,
+                          borderRadius: parseFloat(alert.scan),
+                          backgroundColor: "orange",
                           borderColor: "red",
                           borderWidth: 5,
-                          marginTop: 10, // Adjust the margin as needed
-                          alignSelf: "center", // Center horizontally
+                          marginTop: 10,
+                          alignSelf: "center",
                         }}
                       />
                     )}
                 </>
               ) : null}
 
-              {props.type === "User Alert" ? (
+              {alert.type === "User Alert" ? (
                 <>
                   <Text style={tw.style("text-sm text-white")}>
-                    Message - {a?.message}
+                    Message - {alert.message}
                   </Text>
-                  {/* <Text style={tw.style("text-sm text-white")}>Time - {a?.time}</Text> */}
+                  {/* <Text style={tw.style("text-sm text-white")}>Time - {alert.time}</Text> */}
                   <Text style={tw.style("text-sm text-white")}>
-                    Radius - {a?.radius}{" "}
+                    Radius - {alert.radius}{" "}
                   </Text>
                   {/* Add a View to represent the circle */}
                   {/* <View
                     style={{
-                      width: a?.radius * 2, // Diameter of the circle
-                      height: a?.radius * 2,
-                      borderRadius: a?.radius, // Set borderRadius to half of the width/height to make it a circle
-                      backgroundColor: "orange", // Change the color as needed
+                      width: alert.radius * 2,
+                      height: alert.radius * 2,
+                      borderRadius: alert.radius,
+                      backgroundColor: "orange",
                       borderColor: "red",
                       borderWidth: 5,
-                      marginTop: 10, // Adjust the margin as needed
-                      alignSelf: "center", // Center horizontally
+                      marginTop: 10,
+                      alignSelf: "center",
                     }}
                   /> */}
                 </>
               ) : null}
             </View>
           </View>
-          {/* <Pressable
-            style={tw.style("bg-blue-500 p-2 rounded-md mt-2 self-center")}
-            onPress={() => {
-              props.setJumpToLocation((prevLocation) => ({
-                ...prevLocation,
-                longitude: a.longitude,
-                latitude: a.latitude,
-              }));
-              console.log(a.latitude, a.longitude);
-              console.log(props.jumpToLocation);
-            }}
-          >
-            <Text style={tw.style("text-white text-sm")}>View on Map</Text>
-          </Pressable> */}
+          {/* Pressable logic here */}
         </View>
       ))}
     </View>
   );
 });
+
+// Function to get the appropriate icon based on the alert type
+const getAlertIcon = (type: string) => {
+  switch (type) {
+    case "Fire":
+      return require("../../assets/mapIcons/Wildfire.png");
+    case "Earthquake":
+      return require("../../assets/mapIcons/Earthquake.png");
+    case "Tsunami":
+      return require("../../assets/mapIcons/Tsunami.png");
+    case "User Alert":
+      return require("../../assets/alert-categorys/Hazard.png"); // Adjust as needed
+    default:
+      return null;
+  }
+};
 
 export default ModalViewAlerts;
