97fd974b16f68afdc30049225f0d15b79596d11c
JekBurri <onorbtanko@gmail.com>
2023-11-29 15:14:10 -0800

    Remove unused styles from map component

--- app/map.tsx
+++ app/map.tsx
@@ -24,29 +24,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: "100%",
-    height: "100%",
-  },
-  searchInput: {
-    borderRadius: 62,
-    backgroundColor: "white",
-    padding: 10,
-    marginBottom: 0,
-    marginHorizontal: 10,
-    elevation: 3,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    marginRight: 10,
-  },
-});
