e63ff567ecfccaa3e98cb507e0852363ff0cc1e7
QubeQode <mohammad.razi.fakih@gmail.com>
2023-11-30 19:00:00 -0800

    Implement skeleton load for map

---
+++ app/components/molecules/map-loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import tw from "../../../lib/tailwind";
+
+export default function MapLoading () {
+      return (
+            <View style={tw.style("animate-pulse bg-muted h-[4rem] w-[4rem] rounded-lg")}>
+                  
+            </View>
+      )
+}
